b2a272f153ce6cc8f76b12bc3da3255f28e620f4
reconzo-harsh <tech-intern1@reconzo.com>
2025-12-08 13:27:33 +0530

    fix cors

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import routes from "./routes/index.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/v1", routes);
 
@@ -19,14 +21,3 @@ const MODE = process.env.NODE_ENV || "development";
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} in ${MODE} mode`);
 });
-
-// API Endpoints
-
-// ### Platforms
-// - `POST /api/v1/platforms` - Create platform
-// - `GET /api/v1/platforms` - Get all platforms
-
-//### Clients
-//- `POST /api/v1/clients/:clientName/:platform` - Create client
-//- `GET /api/v1/clients/:clientName` - Get client by name
-//- `GET /api/v1/clients/:clientName/status` - Get client field status

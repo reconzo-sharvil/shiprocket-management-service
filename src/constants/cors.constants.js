d0945019cd09d9738579f1066bc97ea5ec7dbd89
Sharvil Shetty <sharvil.shetty@reconzo.com>
2025-12-18 09:49:22 +0530

    feat(api): add zod validations each api

--- src/constants/cors.constants.js
+++ src/constants/cors.constants.js
@@ -1,9 +1,5 @@
-const isDevelopment = process.env.NODE_ENV === "development";
-
 export const corsOptions = {
-  origin: isDevelopment
-    ? "http://127.0.0.1:5500"
-    : "https://nondescript-idea.surge.sh",
+  origin: "https://nondescript-idea.surge.sh",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
